73e6e10435fc01cd7158e2dad5addac248d46b0e
Priyankaahuja01 <ahuja.pr@northeastern.edu>
2022-04-15 06:14:41 -0400

    last

--- src/main/java/com/example/demo/config/Secure.java
+++ src/main/java/com/example/demo/config/Secure.java
@@ -46,6 +46,8 @@ httpSecurity
 .authorizeRequests()
 .antMatchers(HttpMethod.POST, "/v1/*").permitAll()
 .antMatchers(HttpMethod.GET, "/v1/*").permitAll()
+.antMatchers(HttpMethod.GET, "/").permitAll()
+.antMatchers(HttpMethod.POST, "/").permitAll()
 
 .antMatchers("/healthz").permitAll()
 .antMatchers("/health").permitAll()
